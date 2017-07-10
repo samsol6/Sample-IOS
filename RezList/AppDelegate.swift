98cd73ab068de66541d771d24002d9a0d749c395
smarter-solutions6 <smarter.solutions6@gmail.com>
2017-07-10 19:00:17 +0500

    realist control files added

--- RezList/AppDelegate.swift
+++ RezList/AppDelegate.swift
@@ -9,13 +9,64 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,UITabBarControllerDelegate {
 
     var window: UIWindow?
-
+    var tabBarController: UITabBarController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        let screenWidth = screenSize.width
+        
+        
+        
+        self.tabBarController = UITabBarController()
+//        self.tabBarController?.tabBar.frame = CGRect(x:0, y:0,width:screenWidth, height:50)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = HomeViewController()
+        let searchVc = SearchViewController(nibName: "SearchViewController", bundle: nil)
+        searchVc.tabBarItem.title = "search"
+//        searchVc.tabBarItem.image = UIImage(named: "person.png")?.withRenderingMode(.alwaysOriginal)
+//        searchVc.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        let profileVc = ProfileViewController(nibName: "ProfileViewController", bundle: nil)
+        profileVc.tabBarItem.title = "profile"
+//        profileVc.tabBarItem.image = UIImage(named: "icon-bell.png")?.withRenderingMode(.alwaysOriginal)
+//        profileVc.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        
+        let filterVc = FilterViewController(nibName: "FilterViewController", bundle: nil)
+        filterVc.tabBarItem.title = "Filter"
+        
+        let dashboardVC = DashboardViewController(nibName: "DashboardViewController", bundle: nil)
+        dashboardVC.tabBarItem.title = "Dashboard"
+        
+        let notificationVc = NotificationViewController(nibName: "NotificationViewController", bundle: nil)
+        notificationVc.tabBarItem.title = "notification"
+        
+        let settingVc = SettingViewController(nibName: "SettingViewController", bundle: nil)
+        settingVc.tabBarItem.title = "setting"
+        
+        let supportVc = SupportViewController(nibName: "SupportViewController", bundle: nil)
+        supportVc.tabBarItem.title = "setting"
+        
+        let flatCalVc = FlatFeeCalculatorViewController(nibName: "FlatFeeCalculatorViewController", bundle: nil)
+        flatCalVc.tabBarItem.title = "calculator"
+        
+        let feCal = FeeCalculatorViewController(nibName: "FeeCalculatorViewController", bundle: nil)
+        feCal.tabBarItem.title = "calculator"
+
+        let controllers = [dashboardVC,notificationVc, searchVc,feCal, filterVc]
+        //notificationVc, profileVc, filterVc, dashboardVC,
+        tabBarController?.viewControllers = controllers
+//        window?.rootViewController = tabBarController
+        
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
         return true
     }
 
