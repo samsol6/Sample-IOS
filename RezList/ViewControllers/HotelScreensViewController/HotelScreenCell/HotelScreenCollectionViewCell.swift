165ae7bd950bda1f737e157713dde98bb7156d07
smarter-solutions6 <smarter.solutions6@gmail.com>
2017-07-19 11:28:55 +0500

    collection view cell added for gallery view of hotel screens

---
+++ RezList/ViewControllers/HotelScreensViewController/HotelScreenCell/HotelScreenCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HotelScreenCollectionViewCell.swift
+//  RezList
+//
+//  Created by user 1 on 18/07/2017.
+//  Copyright © 2017 ahmad. All rights reserved.
+//
+
+import UIKit
+
+class HotelScreenCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var hotelCell: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
