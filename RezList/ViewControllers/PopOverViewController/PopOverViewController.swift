5de52402bb47eed516b84a99e53223c6c597e0ae
smarter-solutions6 <smarter.solutions6@gmail.com>
2017-07-25 13:37:48 +0500

    new menu bar added for opening camera to scan QR/BarCodes

---
+++ RezList/ViewControllers/PopOverViewController/PopOverViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PopOverViewController.swift
+//  RezList
+//
+//  Created by user 1 on 24/07/2017.
+//  Copyright © 2017 ahmad. All rights reserved.
+//
+
+import UIKit
+
+class PopOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RezList/ViewControllers/SearchViewController/ScannerTableViewCell/ScannerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScannerTableViewCell.swift
+//  RezList
+//
+//  Created by user 1 on 24/07/2017.
+//  Copyright © 2017 ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ScannerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
